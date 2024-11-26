9d8b3c9b5e752057d45d4d6109f5d6dbc27b6ad0
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2024-11-27 08:44:25 +0900

    add design kit module

--- Platform/Package.swift
+++ Platform/Package.swift
@@ -5,17 +5,19 @@ import PackageDescription
 
 let package = Package(
   name: "Platform",
+  platforms: [
+    .iOS(.v17),
+    .macOS(.v14)
+  ],
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
-      name: "Platform",
-      targets: ["Platform"]),
+      name: "DesignKit",
+      targets: ["DesignKit"]
+    ),
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "Platform"),
-    
+      name: "DesignKit"
+    ),
   ]
 )
