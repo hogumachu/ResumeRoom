3e57e2f06f74a5d8bc2a45deca5820b0ee8b429d
SungJun Hong <castle_jun@daum.net>
2024-12-05 22:34:08 +0900

    add resume id

--- Service/Sources/Entity/Resume.swift
+++ Service/Sources/Entity/Resume.swift
@@ -10,17 +10,20 @@ import SwiftData
 
 @Model
 public final class Resume {
+  public var id: UUID?
   public var title: String?
   public var createdAt: Date?
   public var updatedAt: Date?
   public var contents: String? // TODO: - 변경하기
   
   public init(
+    id: UUID? = nil,
     title: String? = nil,
     createdAt: Date? = nil,
     updatedAt: Date? = nil,
     contents: String? = nil
   ) {
+    self.id = id
     self.title = title
     self.createdAt = createdAt
     self.updatedAt = updatedAt
