c932b672b95db7d3580ca7f813d7c1326eedf50e
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2024-12-06 16:56:28 +0900

    add resume component

---
+++ Service/Sources/ResumeService/Components/ResumeComponent.swift
@@ -0,0 +1,12 @@
+//
+//  ResumeComponent.swift
+//  Service
+//
+//  Created by 홍성준 on 12/6/24.
+//
+
+import Foundation
+
+public enum ResumeComponent: Hashable, Codable {
+  case basic(ResumeBasicComponent)
+}
