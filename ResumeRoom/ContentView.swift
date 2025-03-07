0cfbfd8f6b3eb083b05ca786424962308cfedba0
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2025-03-07 10:50:28 +0900

    initial commit

---
+++ ResumeRoom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ResumeRoom
+//
+//  Created by 홍성준 on 3/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ ResumeRoom/ResumeRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeRoomApp.swift
+//  ResumeRoom
+//
+//  Created by 홍성준 on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ResumeRoomApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
