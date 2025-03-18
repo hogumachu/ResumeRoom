abab5928c5864008c6a154088baadbc456c289f2
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2025-03-19 08:45:10 +0900

    unsupport macos

--- Feature/Package.swift
+++ Feature/Package.swift
@@ -1,34 +1,14 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 typealias Dependency = PackageDescription.Package.Dependency
 typealias TargetDependency = PackageDescription.Target.Dependency
 
-#if os(macOS)
-let dependency: [Dependency] = [
-  .package(path: "../Shared"),
-]
-let calendarDependency: [TargetDependency] = [
-  .product(name: "PresentationKit", package: "Shared"),
-]
-#else
-let dependency: [Dependency] = [
-  .package(path: "../Shared"),
-  .package(url: "https://github.com/airbnb/HorizonCalendar", from: "2.0.0")
-]
-let calendarDependency: [TargetDependency] = [
-  .product(name: "PresentationKit", package: "Shared"),
-  "HorizonCalendar"
-]
-#endif
-
 let package = Package(
   name: "Feature",
   platforms: [
     .iOS(.v18),
-    .macOS(.v15)
   ],
   products: [
     .library(
@@ -44,11 +24,17 @@ let package = Package(
       targets: ["Setting"]
     ),
   ],
-  dependencies: dependency,
+  dependencies: [
+    .package(path: "../Shared"),
+    .package(url: "https://github.com/airbnb/HorizonCalendar", from: "2.0.0"),
+  ],
   targets: [
     .target(
       name: "Calendar",
-      dependencies: calendarDependency
+      dependencies: [
+        .product(name: "PresentationKit", package: "Shared"),
+        "HorizonCalendar",
+      ]
     ),
     .target(
       name: "Home",
--- Feature/Sources/Calendar/Dashboard/Components/CalendarContentView.swift
+++ Feature/Sources/Calendar/Dashboard/Components/CalendarContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 import DesignSystem
 
+import HorizonCalendar
+
 struct CalendarContentView: View {
   
   @Binding var date: Date
@@ -24,15 +26,22 @@ struct CalendarContentView: View {
   }
   
   var body: some View {
-#if os(macOS)
-    CalendarContentMacView(configuration: configuration)
-#else
-    CalendarContentIOSView(
-      date: _date,
-      configuration: configuration,
-      onDaySelection: onDaySelection
+    CalendarViewRepresentable(
+      calendar: configuration.calendar,
+      visibleDateRange: configuration.visibleDateRange,
+      monthsLayout: .horizontal,
+      dataDependency: nil
     )
-#endif
+    .days(days)
+    .onDaySelection { onDaySelection($0.components) }
+    .meshGradientBackground()
+  }
+  
+  private func days(_ day: DayComponents) -> some View {
+    let current = configuration.calendar.date(from: day.components)
+    let isSameDay = current?.isSameDay(date) == true
+    return Text("\(day.day)")
+      .font(isSameDay ? .pretendard(.bold, size: 16) : .pretendard(.regular, size: 16))
   }
   
   private func onDaySelection(_ day: DateComponents) {
@@ -43,56 +52,3 @@ struct CalendarContentView: View {
     date = selectedDate
   }
 }
-
-
-#if !os(macOS)
-import HorizonCalendar
-
-struct CalendarContentIOSView: View {
-  
-  private var date: Binding<Date>
-  
-  private let configuration: CalendarConfiguration
-  private let onDaySelection: (DateComponents) -> Void
-  
-  init(
-    date: Binding<Date>,
-    configuration: CalendarConfiguration,
-    onDaySelection: @escaping (DateComponents) -> Void
-  ) {
-    self.date = date
-    self.configuration = configuration
-    self.onDaySelection = onDaySelection
-  }
-  
-  var body: some View {
-    CalendarViewRepresentable(
-      calendar: configuration.calendar,
-      visibleDateRange: configuration.visibleDateRange,
-      monthsLayout: .horizontal,
-      dataDependency: nil
-    )
-    .days(days)
-    .onDaySelection { onDaySelection($0.components) }
-  }
-  
-  private func days(_ day: DayComponents) -> some View {
-    let current = configuration.calendar.date(from: day.components)
-    let isSameDay = current?.isSameDay(date.wrappedValue) == true
-    return Text("\(day.day)")
-      .font(isSameDay ? .pretendard(.bold, size: 16) : .pretendard(.regular, size: 16))
-  }
-}
-#else
-struct CalendarContentMacView: View {
-  private let configuration: CalendarConfiguration
-  
-  init(configuration: CalendarConfiguration) {
-    self.configuration = configuration
-  }
-  
-  var body: some View {
-    Text("TBU")
-  }
-}
-#endif
